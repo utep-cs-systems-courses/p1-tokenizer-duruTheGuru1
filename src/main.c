c23d3327f8b670ad2f5eac2dbd1eb4d789f7b558
duruTheGuru1 <dduru@miners.utep.edu>
2023-07-27 01:04:50 -0600

    updated tokenizer

--- src/main.c
+++ src/main.c
@@ -2,17 +2,21 @@
 #include <stdio.h>
 int main(){
 
- 
   char *lie = " a";
   char *les = " ";
   char *lrk = " hello guys";
+  printf("test one\n");
   char *dll = token_start(lrk);
+  printf("test two\n");
   char *sf2 = token_terminator(dll);
+  printf("test three\n");
   int ha = space_char(*lie);
+  printf("test four\n");
   int haha = non_space_char(*lie);
-
-  printf("%s, \n", dll);
-  printf("%s\n", sf2);
+  // int tts = count_tokens(lrk);
+   printf("%s, \n", dll);
+   printf("%s\n", sf2);
+   // printf("%d\n", tts);
   
   //to traverse a char, you use ++; to increment. Ex. str = "my_mom!" str++; "y_mom"
   
